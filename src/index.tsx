c8eb3c0f012fe1f198ac49337ea955f08977120d
Adam Jenkins <adam@boosted.technology.com>
2023-08-30 20:34:18 -0300

    GH pages

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import { Alert } from "@inkblot-therapy/squid";
-
-ReactDOM.render(
-  <Alert title="Work me" show />,
-  document.getElementById("root")
-);
